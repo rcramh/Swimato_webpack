964cd2f553a725d19dc28f9dd164f551581506e1
Rahul Chaubey <131397354+rcramh@users.noreply.github.com>
2024-05-23 00:56:09 +0530

    Create Checkout.js
    creating checkout page

---
+++ src/components/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Checkout = () => {
+  return (
+    <>
+       <h2>Checkout page for placing order</h2>
+      <div>Checkout Page creation under progress ...</div>
+      <div>will add payment gateway integration, sum total amount</div>
+    
+    </>
+    
+  )
+}
+
+export default Checkout
